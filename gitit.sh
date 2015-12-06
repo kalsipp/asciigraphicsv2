dbc18e3bf09d16c42af0cc833d01d4ceef8c93e1
Martin <martin.favre22@gmail.com>
2015-12-06 15:24:59 +0100

    Converting into gameobject based system

--- gitit.sh
+++ gitit.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
-git add textgraphics.cpp
-git add textgraphics.hpp
-git add main.cpp
-git add makefile
-git add run.sh
-git add gitit.sh
-git add gitupdate.sh
-git add imgcontainer.cpp
-git add imgcontainer.hpp
-git add img_conv_stolen.py
-git add img_output
-git add gameobject.cpp
-git add gameobject.hpp
-git add pixel.hpp
-git add pixel.cpp
-git add textbox.cpp
-git add imgtext3.jpg
+#git add textgraphics.cpp
+#git add textgraphics.hpp
+#git add main.cpp
+#git add makefile
+#git add run.sh
+#git add gitit.sh
+#git add gitupdate.sh
+#git add imgcontainer.cpp
+#git add imgcontainer.hpp
+#git add img_conv_stolen.py
+#git add img_output
+#git add gameobject.cpp
+#git add gameobject.hpp
+#git add pixel.hpp
+#git add pixel.cpp
+#git add textbox.cpp
+#git add imgtext3.jpg
+git add *
 git commit -m "Converting into gameobject based system"
 git push upstream master
